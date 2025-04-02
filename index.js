149f578df56c34253082dbebc6e2798cdeb73325
Reece De Padua <reecedepadua@gmail.com>
2025-04-02 14:06:18 +0800

    ejs added and created no recipes middleware

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import express from "express";
+
 import path from "node:path";
 
 const port = 3000;
-
 const foodBlog = express();
-
 const dirname = import.meta.dirname;
+
+// Set up EJS
+foodBlog.set("view engine", "ejs");
+foodBlog.set("views", path.join(dirname, "views"));
+
+// Serve static files
 foodBlog.use(express.static(path.join(dirname, "public")));
-const homePath = path.join(dirname, "public/index.html");
+
+let recipes = {};
+let hasRecipes = false;
+
+// Middleware that checks if there are already recipes created
+const checkRecipes = (req, res, next) => {
+  hasRecipes = Object.keys(recipes).length > 0;
+  next();
+};
+
+foodBlog.use(checkRecipes);
 
 foodBlog.get("/", (req, res) => {
-  res.sendFile(homePath);
+  res.render("index.ejs", { hasRecipes: hasRecipes });
 });
 
 foodBlog.listen(port, () => {
