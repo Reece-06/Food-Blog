82792cface64645bd42cb0d20e309c1dc90b516c
Reece De Padua <reecedepadua@gmail.com>
2025-04-02 12:48:03 +0800

    added node and basic express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import express from "express";
+import path from "node:path";
 
+const port = 3000;
+
+const foodBlog = express();
+
+const dirname = import.meta.dirname;
+foodBlog.use(express.static(path.join(dirname, "public")));
+const homePath = path.join(dirname, "public/index.html");
+
+foodBlog.get("/", (req, res) => {
+  res.sendFile(homePath);
+});
+
+foodBlog.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
