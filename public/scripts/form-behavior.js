f5e57bdb6474ceebb6f5ad347dc76886b96f0345
Reece De Padua <reecedepadua@gmail.com>
2025-04-04 10:49:38 +0800

    added form behaviors

--- public/scripts/form-behavior.js
+++ public/scripts/form-behavior.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nextBtn = document.querySelector(".next-btn");
   const createRecipeform = document.querySelector(".create-form");
+  const formLabel = document.querySelector(".form-label");
+  const nextButton = document.querySelector(".next-btn");
+  const nextBtnText = document.querySelector(".next-button-text");
   // Changes the btn id
   const increaseBtnId = (btnId) => {
     const splitBtnId = btnId.split("-");
@@ -25,12 +28,55 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     return btnIdObj;
   };
+  const changeButtonLabel = (currentPageNum) => {
+    const buttonLabel = ["Next", "Submit"];
 
+    if (currentPageNum === 4) {
+      nextBtnText.textContent = buttonLabel[1];
+      nextBtn.classList.add("hide-arrow-icon");
+    } else {
+      if (nextBtnText.textContent !== buttonLabel[0]) {
+        console.log("not next text");
+        nextBtnText.textContent = buttonLabel[0];
+      }
+
+      if (nextButton.classList.contains("hide-arrow-icon")) {
+        console.log("hidden arrow");
+        nextBtn.classList.remove("hide-arrow-icon");
+      }
+    }
+  };
+  const changeFormLabel = (
+    currentPageNum,
+    firstLabel,
+    secondLabel,
+    thirdLabel,
+    fourthLabel
+  ) => {
+    if (currentPageNum === 2) {
+      return secondLabel;
+    } else if (currentPageNum === 3) {
+      return thirdLabel;
+    } else if (currentPageNum === 4) {
+      return fourthLabel;
+    } else {
+      return firstLabel; // Default case
+    }
+  };
   // Shows the next form and hides the previous
   const showNextForm = (event) => {
-    const btnId = nextBtn.id;
+    const formLabelArr = [
+      "Basic Info",
+      "Ingredients",
+      "Instructions",
+      "Nutrition",
+    ];
 
-    const btnIdObj = increaseBtnId(btnId);
+    const btnId = nextBtn.id;
+    const btnIdObj = increaseBtnId(btnId); // button id info
+
+    const newFormLabel = changeFormLabel(btnIdObj.lastNum, ...formLabelArr);
+    formLabel.textContent = newFormLabel; // changes form label text
 
     nextBtn.id = btnIdObj.newBtnId;
 
@@ -39,9 +85,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const newShowFormClass = newShowFormArr.join("-");
     const oldShowFormArr = [...showFormArr, btnIdObj.prevNum];
     const oldShowFormClass = oldShowFormArr.join("-");
-    console.log(typeof oldShowFormClass);
+
     createRecipeform.classList.remove(oldShowFormClass);
     createRecipeform.classList.add(newShowFormClass);
+
+    changeButtonLabel(btnIdObj.lastNum);
   };
 
   nextBtn.addEventListener("click", showNextForm);
