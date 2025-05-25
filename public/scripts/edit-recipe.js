600ae7a81f83fe3bdef906a8ebb1742085ef110f
Reece De Padua <reecedepadua@gmail.com>
2025-05-25 11:53:23 +0800

    adding additional instruction inputs working

--- public/scripts/edit-recipe.js
+++ public/scripts/edit-recipe.js
@@ -12,28 +12,53 @@ const retreiveData = async (recipeId) => {
     return null;
   }
 };
+// Add instruction inputs based on the instruction length
+const addInstructionInputs = (insLen) => {
+  console.log(insLen);
+  let numOfExistingIns = 1; // Default 1 existing
+  while (numOfExistingIns < insLen) {
+    const insContainerEls = document.querySelectorAll(
+      ".instruction-input-container"
+    );
+    const lastInsContainer = insContainerEls[insContainerEls.length - 1];
+    console.log("lastInsContainer:", lastInsContainer);
+    const newInsContainer = lastInsContainer.cloneNode(true);
+    console.log("newInsContainer:", newInsContainer);
+    const newInsInput = newInsContainer.querySelector(".instruction-input");
+    console.log("newInsInput:", newInsInput);
+    const newInsLabel = newInsContainer.querySelector(".ins-label");
+    console.log("newInsLabel:", newInsLabel);
+    const idArr = newInsInput.id.split("-");
+    const lastNum = Number(idArr[idArr.length - 1]);
+    console.log("lastNum:", lastNum);
+
+    newInsInput.id = "instruction-" + (lastNum + 1);
+    newInsInput.name = "instruction-" + (lastNum + 1);
+    newInsLabel.setAttribute("for", "instruction-" + (lastNum + 1));
+    console.log("newInsInput:", newInsInput);
+    console.log("newInsLabel:", newInsLabel);
+    lastInsContainer.parentNode.insertBefore(
+      newInsContainer,
+      lastInsContainer.nextSibling
+    );
+    numOfExistingIns++;
+  }
+};
+// Populate instructions
+const populateInstructions = (instructions) => {
+  const instructionLen = instructions.length;
+  addInstructionInputs(instructionLen);
+};
 // Populate times cooktime or preptime
 const populateTimeInputs = (timeArr, timeType) => {
   timeArr.forEach((el, i) => {
     let timeValue = Number(timeArr[i - 1]);
     if (el === "hrs") {
-      if (timeType === "prepTime") {
-        document.querySelector("#prep-time-hours").value = timeValue;
-      } else {
-        document.querySelector("#cook-time-hours").value = timeValue;
-      }
+      document.querySelector("#" + timeType + "-hours").value = timeValue;
     } else if (el === "mins") {
-      if (timeType === "prepTime") {
-        document.querySelector("#prep-time-minutes").value = timeValue;
-      } else {
-        document.querySelector("#cook-time-minutes").value = timeValue;
-      }
+      document.querySelector("#" + timeType + "-minutes").value = timeValue;
     } else {
-      if (timeType === "prepTime") {
-        document.querySelector("#prep-time-seconds").value = timeValue;
-      } else {
-        document.querySelector("#cook-time-seconds").value = timeValue;
-      }
+      document.querySelector("#" + timeType + "-seconds").value = timeValue;
     }
   });
 };
@@ -41,8 +66,8 @@ const populateTimeInputs = (timeArr, timeType) => {
 const populateFormWithData = (recipe) => {
   const cookTimeArr = recipe.cookTime.split(" ");
   const prepTimeArr = recipe.prepTime.split(" ");
-  populateTimeInputs(cookTimeArr, "cookTime");
-  populateTimeInputs(prepTimeArr, "prepTime");
+  populateTimeInputs(cookTimeArr, "cook-time");
+  populateTimeInputs(prepTimeArr, "prep-time");
   document.querySelector("#recipe-name").value = recipe.recipeName;
   document.querySelector("#author").value = recipe.author;
   document.querySelector("#meal-course").value = recipe.mealCourse;
@@ -59,12 +84,13 @@ const populateFormWithData = (recipe) => {
   document.querySelector("#nutrition-protein").value = recipe.protein;
 
   document.querySelector("#nutrition-fat").value = recipe.fat;
+  populateInstructions(recipe.instructions);
 };
 // Identifies which recipe to edit
 const handleEdit = async (recipeId) => {
   const recipeData = await retreiveData(recipeId);
   populateFormWithData(recipeData);
-  console.log(recipeData);
+
   showCreateRecipeModal();
 };
 
